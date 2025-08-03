f96cb1da32c9db0e01c79880e87c74e5384d86b0
Henry Loveday <henrygad.orji@gmail.com>
2025-08-03 15:55:54 +0100

    adjusted dashboard layout component

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { Route, Routes, Navigate } from "react-router-dom";
-import { Suspense } from "react";
+import { Suspense, useEffect } from "react";
 import {
   DashboardPage,
   SettingsPage,
@@ -16,32 +16,74 @@ import {
   ProductDetailsPage,
   LoginPage,
 } from "./pages";
-import { useAppSelector } from "./store";
+import { useAppDispatch, useAppSelector } from "./store";
 import DashboardLayout from "./components/DashboardLayout";
+import { fetchAdminInfo, status } from "./store/slices/userSlice";
 
 const App = () => {
   const user = useAppSelector((state) => state.user);
+  const appDispatch = useAppDispatch();
 
-  return <Suspense fallback={<div>Lazy Loading...</div>}>
-    <Routes>
-      <Route path="/login" element={<LoginPage />} />
-      <Route path="/" element={<DashboardLayout />}>
-        <Route index element={<DashboardPage />} />
-        <Route path="/products" element={<ProductsPage />} />
-        <Route path="/orders" element={<OrdersPage />} />
-        <Route path="/add-product" element={<AddProductPage />} />
-        <Route path="/customers" element={<CustomersPage />} />
-        <Route path="/notifications" element={<NotificationsPage />} />
-        <Route path="/discounts" element={<DiscountsPage />} />
-        <Route path="/add-discount" element={<AddDiscountsPage />} />
-        <Route path="/order-details/:id" element={<OrderDetailsPage />} />
-        <Route path="/analytics" element={<AnalyticsPage />} />
-        <Route path="/product-details/:id" element={<ProductDetailsPage />} />
-        <Route path="/settings" element={<SettingsPage />} />
-      </Route>
-      <Route path="*" element={<NotFoundPage />} />
-    </Routes>
-  </Suspense>;
+  useEffect(() => {
+    // Check whether user cookie session
+    // in the backend is till valid on refresh.
+    // This will return an object containing
+    //  {email, username or id}, to prove user is till login from the backend
+    appDispatch(status({ status: "UNAUTHENTICATED" }));    
+  }, []);
+
+
+  useEffect(() => {
+    if (user.status !== "AUTHENTICATED") return;
+
+      appDispatch(fetchAdminInfo({
+        adminInfo: {
+          name: "mrs admin",
+          isAdmin: true,
+          profilePhoto: "mrsadmin.png"
+        }
+      }));
+    
+  }, [user.status]);
+
+  return (
+    <div className="relative font-main size-full h-screen flex flex-col bg-white group/design-root overflow-hidden">
+      <div className="layout-container flex-1 flex h-full grow flex-col">
+        <Suspense fallback={<div>Lazy Loading...</div>}>
+          <Routes>
+            <Route
+              path="/login"
+              element={
+                user.status === "UNAUTHENTICATED" ? (
+                  <LoginPage />
+                ) : (
+                  <Navigate to="/" />
+                )
+              }
+            />
+            <Route path="/" element={<DashboardLayout />}>
+              <Route index element={<DashboardPage />} />
+              <Route path="/products" element={<ProductsPage />} />
+              <Route path="/orders" element={<OrdersPage />} />
+              <Route path="/add-product" element={<AddProductPage />} />
+              <Route path="/customers" element={<CustomersPage />} />
+              <Route path="/notifications" element={<NotificationsPage />} />
+              <Route path="/discounts" element={<DiscountsPage />} />
+              <Route path="/add-discount" element={<AddDiscountsPage />} />
+              <Route path="/order-details/:id" element={<OrderDetailsPage />} />
+              <Route path="/analytics" element={<AnalyticsPage />} />
+              <Route
+                path="/product-details/:id"
+                element={<ProductDetailsPage />}
+              />
+              <Route path="/settings" element={<SettingsPage />} />
+            </Route>
+            <Route path="*" element={<NotFoundPage />} />
+          </Routes>
+        </Suspense>
+      </div>
+    </div>
+  );
 };
 
 export default App;
--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,69 +1,76 @@
 import { useAppDispatch } from "../store";
 import { login } from "../store/slices/userSlice";
 import Image from "../assets/images/bethora-logo.png";
+
 const LoginPage = () => {
   const appDispatch = useAppDispatch();
+
+
   const handleLoginForm = (e: React.FormEvent) => {
     e.preventDefault();
     appDispatch(login({ email: "henrygad.orji@gmail.com" }));
   };
 
-
-  return <div className="relative font-main size-full h-screen flex flex-col bg-white group/design-root overflow-hidden">
-    <div className="layout-container flex-1 flex h-full grow flex-col">
-      <header className="border-b border-solid border-b-[#f4f0f0] px-10 py-3">
-      </header>
-      <main className="flex-1 flex overflow-hidden">
-        <div className="flex flex-1 justify-center py-4 sm:py-6">
-          <div className="flex flex-col max-w-[960px] flex-1">
-            <h2 className="text-[#171212] tracking-light text-[28px] font-bold leading-tight px-4 text-center pb-3 pt-5">
-              Welcome back
-            </h2>
-            <img src={Image} alt="Bethora Logo" />
-            <form
-              onSubmit={handleLoginForm}
-            >
-              <div className="flex max-w-[480px] flex-wrap items-end gap-4 px-4 py-3">
-                <label className="flex flex-col min-w-40 flex-1">
-                  <p className="text-[#171212] text-base font-medium leading-normal pb-2">
-                    Username
-                  </p>
-                  <input
-                    placeholder="Enter your username"
-                    className="form-input flex w-full min-w-0 flex-1 resize-none overflow-hidden rounded-xl text-[#171212] focus:outline-0 focus:ring-0 border-none bg-[#f4f1f1] focus:border-none h-14 placeholder:text-[#82686a] p-4 text-base font-normal leading-normal"
-                    value=""
-                  />
-                </label>
-              </div>
-              <div className="flex max-w-[480px] flex-wrap items-end gap-4 px-4 py-3">
-                <label className="flex flex-col min-w-40 flex-1">
-                  <p className="text-[#171212] text-base font-medium leading-normal pb-2">
-                    Password
-                  </p>
-                  <input
-                    placeholder="Enter your password"
-                    className="form-input flex w-full min-w-0 flex-1 resize-none overflow-hidden rounded-xl text-[#171212] focus:outline-0 focus:ring-0 border-none bg-[#f4f1f1] focus:border-none h-14 placeholder:text-[#82686a] p-4 text-base font-normal leading-normal"
-                    value=""
-                  />
-                </label>
-              </div>
-              <div className="flex px-4 py-3">
-                <button className="flex min-w-[84px] max-w-[480px] cursor-pointer items-center justify-center overflow-hidden rounded-full h-10 px-4 flex-1 bg-[#e8b4b7] text-[#171212] text-sm font-bold leading-normal tracking-[0.015em]">
-                  <span className="truncate">Login</span>
-                </button>
-              </div>
-            </form>
-            <p className="text-[#82686a] text-sm font-normal leading-normal pb-3 pt-1 px-4 text-center underline">
-              Forgot Password?
-            </p>
+  return <>
+    <header className="">
+    </header>
+    <main className="flex-1 flex overflow-hidden">
+      <div className="flex flex-1 justify-center py-4 sm:py-6">
+        <div className="flex-1 flex flex-col max-w-[960px] justify-center items-center">
+          <div className="flex flex-col-reverse sm:flex-row bg-white rounded-xl shadow-xl min-w-[320px]">
+            <div className="flex-1 p-4">
+              <h2 className="text-[#171212] tracking-light text-[28px] font-bold leading-tight px-4 text-center pb-3 pt-5">
+                Welcome back
+              </h2>
+              <form
+                className="w-full"
+                onSubmit={handleLoginForm}
+              >
+                <div className="flex max-w-[480px] flex-wrap items-end gap-4 px-4 py-3">
+                  <label className="flex flex-col min-w-40 flex-1">
+                    <p className="text-[#171212] text-base font-medium leading-normal pb-2">
+                      Username
+                    </p>
+                    <input
+                      placeholder="Enter your username"
+                      className="form-input flex w-full min-w-0 flex-1 resize-none overflow-hidden rounded-xl text-[#171212] focus:outline-0 focus:ring-0 border-none bg-[#f4f1f1] focus:border-none h-14 placeholder:text-[#82686a] p-4 text-base font-normal leading-normal"
+                      value=""
+                    />
+                  </label>
+                </div>
+                <div className="flex max-w-[480px] flex-wrap items-end gap-4 px-4 py-3">
+                  <label className="flex flex-col min-w-40 flex-1">
+                    <p className="text-[#171212] text-base font-medium leading-normal pb-2">
+                      Password
+                    </p>
+                    <input
+                      placeholder="Enter your password"
+                      className="form-input flex w-full min-w-0 flex-1 resize-none overflow-hidden rounded-xl text-[#171212] focus:outline-0 focus:ring-0 border-none bg-[#f4f1f1] focus:border-none h-14 placeholder:text-[#82686a] p-4 text-base font-normal leading-normal"
+                      value=""
+                    />
+                  </label>
+                </div>
+                <div className="flex px-4 py-3">
+                  <button className="flex min-w-[84px] max-w-[480px] cursor-pointer items-center justify-center overflow-hidden rounded-full h-10 px-4 flex-1 bg-[#e8b4b7] text-[#171212] text-sm font-bold leading-normal tracking-[0.015em]">
+                    <span className="truncate">Login</span>
+                  </button>
+                </div>
+              </form>
+              <p className="text-[#82686a] text-sm font-normal leading-normal pb-3 pt-1 px-4 text-center underline">
+                Forgot Password?
+              </p>
+            </div>
+            <div className="flex-1 flex justify-center px-4 pt-6  sm:p-0">
+              <img src={Image} alt="Bethora Logo" className="object-cover size-16 rounded-full sm:rounded-none sm:size-full" />
+            </div>
           </div>
         </div>
-      </main>
-      <footer className="w-full flex items-center overflow-hidden">
+      </div>
+    </main>
+    <footer className="w-full flex items-center overflow-hidden">
 
-      </footer>
-    </div>
-  </div>;
+    </footer>
+  </>;
 };
 
 export default LoginPage;
