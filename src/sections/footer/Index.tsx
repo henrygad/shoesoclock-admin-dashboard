06a0ae58de3e20a50bb026e5cb0554a7dacd9561
Henry Loveday <henrygad.orji@gmail.com>
2025-08-13 09:52:19 +0100

    notification data in assest

--- src/sections/footer/Index.tsx
+++ src/sections/footer/Index.tsx
@@ -2,8 +2,7 @@
 const Footer = () => {
   return (
     <>
-      <nav className="flex-1 flex md:hidden justify-start w-full overflow-hidden bg-transparent p-1.5">
-        
+      <nav className="flex-1 flex md:hidden justify-start w-full overflow-hidden bg-transparent p-1.5">      
       </nav>
     </>
   );
